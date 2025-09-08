44b08e2158419b4ccd84d7d44ec780f75c9f511a
Mr-Steve-H <felixiemoje@gmail.com>
2025-09-08 13:00:13 +0000

    wip

--- Opdrachten/PyGame/Les1/Les1.py
+++ Opdrachten/PyGame/Les1/Les1.py
@@ -16,8 +16,22 @@ pygame.display.set_caption('Eerste game!')
 clock = pygame.time.Clock()
 running = True
 
-surface = pygame.Surface((100, 100))
+surface = pygame.Surface((800, 400))
 surface.fill("blue")
+pipe1 = pygame.Surface((50, 200))
+pipe1.fill('Green')
+pipe2 = pygame.Surface((50, 100))
+pipe2.fill('Green')
+pipe3 = pygame.Surface((50, 250))
+pipe3.fill('Green')
+pipe4 = pygame.Surface((50, 50))
+pipe4.fill('Green')
+pipe5 = pygame.Surface((50, 50))
+pipe5.fill('Green')
+pipe6 = pygame.Surface((50, 250))
+pipe6.fill('Green')
+flappy = pygame.Surface((20, 20))
+flappy.fill('Yellow')
 
 while running:
 
@@ -26,6 +40,14 @@ while running:
       running = False
 
   screen.blit(surface, (0, 0))
+  screen.blit(pipe1, (200, 0))
+  screen.blit(pipe2, (200, 300))
+  screen.blit(pipe3, (400, 0))
+  screen.blit(pipe4, (400, 350))
+  screen.blit(pipe5, (600, 0))
+  screen.blit(pipe6, (600, 150))
+  screen.blit(flappy, (100, 200))
+
   
   pygame.display.update()
   clock.tick(60)
