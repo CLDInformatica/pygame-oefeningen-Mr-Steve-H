2c40888597e03e199f8eec55637b0fca3e192fe4
Mr-Steve-H <felixiemoje@gmail.com>
2025-09-12 07:52:19 +0000

    Les 3 Af

--- Opdrachten/PyGame/Les3/les3.py
+++ Opdrachten/PyGame/Les3/les3.py
@@ -24,6 +24,16 @@ background_surface.fill("white")
 auto_surface = pygame.image.load("Opdrachten/PyGame/Les3/graphics/auto.png").convert_alpha()
 auto_x_pos = 200
 
+rain = pygame.image.load("Opdrachten/PyGame/Les3/graphics/purepng.com-raindropsnaturestylenaturalbeautifulgreen-541521126280eg699.png").convert_alpha()
+Scale1 = (400, 200)
+rain = pygame.transform.scale(rain, Scale1)
+rain_y_pos = -200
+rain_y_pos2 = 0
+rain_y_pos3 = -200
+rain_y_pos4 = 0
+rain_y_pos5 = 200
+rain_y_pos6 = 200
+
 while running:
 
     for event in pygame.event.get():
@@ -34,6 +44,35 @@ while running:
 
     auto_x_pos += 1
     screen.blit(auto_surface, (auto_x_pos, 200))
+    if auto_x_pos == 800:
+      auto_x_pos = -200
+
+    screen.blit(rain, (0, rain_y_pos))
+    screen.blit(rain, (0, rain_y_pos2))
+    screen.blit(rain, (400, rain_y_pos3))
+    screen.blit(rain, (400, rain_y_pos4))
+    screen.blit(rain, (0, rain_y_pos5))
+    screen.blit(rain, (400, rain_y_pos6))
+
+    rain_y_pos += 1
+    rain_y_pos2 += 1
+    rain_y_pos3 += 1
+    rain_y_pos4 += 1
+    rain_y_pos5 += 1
+    rain_y_pos6 += 1
+
+    if rain_y_pos == 400:
+      rain_y_pos = -200
+    if rain_y_pos2 == 400:
+      rain_y_pos2 = -200
+    if rain_y_pos3 == 400:
+      rain_y_pos3 = -200
+    if rain_y_pos4 == 400:
+      rain_y_pos4 = -200
+    if rain_y_pos5 == 400:
+      rain_y_pos5 = -200
+    if rain_y_pos6 == 400:
+      rain_y_pos6 = -200
 
     pygame.display.update()
     clock.tick(60)
